e8c484baebcc5fa0922e387163cf98106958d592
sebaso <sebastian.olaechea.so@gmail.com>
2025-12-15 17:24:26 +0100

    Game

--- Assets/Scripts/Lógica/Figuras.cs
+++ Assets/Scripts/Lógica/Figuras.cs
@@ -5,6 +5,8 @@ public class Figuras : MonoBehaviour
     public float timer;
     public int TiempoLimite;
     public bool isCorrrect;
+    public bool hasBeenPicked;
+    private Vector3 originalPosition;
     // Esta es la clase de la Figura. Toda figura tiene estas propiedades y métodos.
     // Cuando una figura tiene que ser elegida por el jugador, se puede coger con el RaycasterGrabber
     // y se debe arrastrar hasta el mantel de venta.
@@ -12,17 +14,23 @@ public class Figuras : MonoBehaviour
     // y te ha dicho cual de las figuras quiere. Solo puedes coger esa (brillará) y arrastrarla hasta el mantel de venta.
     void Start()
     {
-        
+        hasBeenPicked = false;
+        originalPosition = gameObject.transform.position;
     }
 
 
     void Update()
     {
-        timer+= Time.deltaTime;
-        if(timer>= TiempoLimite)
+        if (!hasBeenPicked)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= TiempoLimite)
         {
             print("Respwaneando");
-            timer=0;
+            timer = 0;
+            gameObject.transform.position = originalPosition;
+            hasBeenPicked = false;
         }
         if (isCorrrect)
         {
--- Assets/Scripts/Player/RaycasterGrabber.cs
+++ Assets/Scripts/Player/RaycasterGrabber.cs
@@ -13,18 +13,18 @@ public class RaycasterGrabber : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        camera=Camera.main;
+        camera = Camera.main;
     }
     public void GrabObject()
     {
         if (grabbedObject)
         {
-            
-        
-        grabbedObject.transform.position = camera.transform.position + camera.transform.forward * grabDistance;
-        grabbedObject.GetComponent<Rigidbody>().useGravity=false;
-        grabbedObject.GetComponent<Rigidbody>().isKinematic=false;
-       
+
+
+            grabbedObject.transform.position = camera.transform.position + camera.transform.forward * grabDistance;
+            grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+
         }
     }
 
@@ -34,26 +34,28 @@ public class RaycasterGrabber : MonoBehaviour
         RaycastHit raycast;
         if (grabbedObject && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            grabbedObject.GetComponent<Rigidbody>().useGravity=true;
-            grabbedObject.GetComponent<Rigidbody>().linearVelocity = new Vector3(0,0,0);
-            grabbedObject=null;
-            
+            grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+            grabbedObject.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, 0);
+            grabbedObject.GetComponent<Figuras>().hasBeenPicked = false;
+            grabbedObject = null;
+
             return;
         }
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if(Physics.Raycast(ray,out raycast))
+            if (Physics.Raycast(ray, out raycast))
             {
 
-           if (raycast.collider.gameObject.tag=="objeto")
-            {
-                //print("hola");
-                grabbedObject=raycast.transform.gameObject;
+                if (raycast.collider.gameObject.tag == "objeto")
+                {
+                    //print("hola");
+                    grabbedObject = raycast.transform.gameObject;
+                    grabbedObject.GetComponent<Figuras>().hasBeenPicked = true;
+                }
             }
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.white);
         }
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.white);
+        GrabObject();
     }
-    GrabObject();
-}
 }
